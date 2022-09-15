97165f20485024170ad4cc0c30e1eaeb6f51a26d
unknown <mindc1july111@iiht.tech>
2022-09-15 13:32:44 +0530

    gm

---
+++ F1/Online E-Grocecry/Core/Interface/IUser.cs
@@ -0,0 +1,16 @@
+﻿using Online_E_Grocecry.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_E_Grocecry.Core.Interface
+{
+   public interface IUser
+    {
+        List<User>displayUser();
+        User Post(User user);
+        User Put(User user);
+        bool Delete(int userId);
+    }
+}
